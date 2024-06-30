56863ae006c5a04f82bc14587383d18270f57bc9
Shauna Moore <shaunamoore1226@gmail.com>
2024-06-29 21:30:52 -0400

    heyNow complete

--- ACME_System/src/ver1/HelloWorld.java
+++ ACME_System/src/ver1/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
 		System.out.println("Shauna is working locally on her branch");
 
 	}
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
+	
 
 }
